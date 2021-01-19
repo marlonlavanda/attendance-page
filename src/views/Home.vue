ee6c0f34d93a0f08899e1c7260bce43413cf4ce9
Marlon Lavanda <marlonlavanda@hotmail.com>
2021-01-19 22:51:09 +0000

    done

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,42 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <Header></Header>
+    <div class="bv-example-row">
+      <b-row class="text-center mt-5">
+        <b-col cols="4">
+          <SideBar></SideBar>
+        </b-col>
+        <b-col>
+          <b-button variant="danger" class="mb-3">Button</b-button>
+          <Table :asset="asset"></Table>
+        </b-col>
+      </b-row>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Table from "../components/table.vue";
+import SideBar from "../components/side-bar.vue";
+import Header from "../components/header.vue";
+
+import api from "../api.js";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
-  }
-}
+    Table,
+    SideBar,
+    Header,
+  },
+  data() {
+    return {
+      asset: [],
+    };
+  },
+  created() {
+    api.getAssets().then((asset) => (this.asset = asset));
+  },
+};
 </script>
